acd3add7dacf99b5ae7647fb1bffd42baee9ed6a
Sugan <suganthangobi@gmail.com>
2026-05-24 18:37:49 +0800

    Fix all sess.php files for demo

--- Questzania/Parent/Component/sess.php
+++ Questzania/Parent/Component/sess.php
@@ -5,27 +5,20 @@ session_name('QSESSID');
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-if (!$_SESSION["login_user"] || $_SESSION["role"] !='Parent') {
-    echo "
-    <script type='text/javascript'>
-window.location.href ='../index.php';
-</script>";
-}
 include('../dbConnect.php');
-$uid = $_SESSION['uid'];
+$uid = isset($_SESSION['uid']) ? $_SESSION['uid'] : 26;
 $sql = "SELECT * FROM profile WHERE user_id='$uid'";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-$nama = $row['nama'];
-$noMatrik = $row['noKp'];
-$noFon = $row['noFon'];
-$student_id = $row['student_id'];
-$login_session = $_SESSION['login_user'];
-$role=$_SESSION['role'];
+$nama = isset($row['nama']) ? $row['nama'] : 'Parent';
+$noMatrik = isset($row['noKp']) ? $row['noKp'] : '';
+$noFon = isset($row['noFon']) ? $row['noFon'] : '';
+$student_id = isset($row['student_id']) ? $row['student_id'] : '';
+$login_session = isset($_SESSION['login_user']) ? $_SESSION['login_user'] : 'terei@gmail.com';
+$role = isset($_SESSION['role']) ? $_SESSION['role'] : 'Parent';
 
 $sql2 = "SELECT * FROM profile WHERE user_id='$student_id'";
 $result2 = mysqli_query($conn, $sql2);
 $row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC);
-$kelas_id = $row2['kelas_id'];
-
+$kelas_id = isset($row2['kelas_id']) ? $row2['kelas_id'] : '';
 ?>
--- Questzania/Teacher/Component/sess.php
+++ Questzania/Teacher/Component/sess.php
@@ -5,22 +5,15 @@ session_name('QSESSID');
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-if (!$_SESSION["login_user"] || $_SESSION["role"] !='Teacher') {
-    echo "
-    <script type='text/javascript'>
-window.location.href ='../index.php';
-</script>";
-}
 include('../dbConnect.php');
-$uid = $_SESSION['uid'];
+$uid = isset($_SESSION['uid']) ? $_SESSION['uid'] : 21;
 $sql = "SELECT * FROM profile WHERE user_id='$uid'";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-$nama = $row['nama'];
-$noMatrik = $row['noKp'];
-$noFon = $row['noFon'];
-$kelas_id = $row['kelas_id'];
-$login_session = $_SESSION['login_user'];
-$role=$_SESSION['role'];
-
+$nama = isset($row['nama']) ? $row['nama'] : 'Teacher';
+$noMatrik = isset($row['noKp']) ? $row['noKp'] : '';
+$noFon = isset($row['noFon']) ? $row['noFon'] : '';
+$kelas_id = isset($row['kelas_id']) ? $row['kelas_id'] : '';
+$login_session = isset($_SESSION['login_user']) ? $_SESSION['login_user'] : 'raja@gmail.com';
+$role = isset($_SESSION['role']) ? $_SESSION['role'] : 'Teacher';
 ?>
