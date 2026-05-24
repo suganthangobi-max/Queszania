22b11ca566e6c565f8497afb3a57686acb4d2e96
Sugan <suganthangobi@gmail.com>
2026-05-24 16:22:15 +0800

    Revert to mysqli

--- Questzania/dbConnect.php
+++ Questzania/dbConnect.php
@@ -5,11 +5,9 @@ $password = "UlkLxQTzEEzAIDbKWKQhpgKkOtoVobXR";
 $database = "railway";
 $port = 3306;
 
-try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$database", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn = $pdo;
-} catch(PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+$conn = mysqli_connect($host, $user, $password, $database, $port);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
