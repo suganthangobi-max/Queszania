fa3170e23bf5c0e022e968b6daa0e18177bc01af
Sugan <suganthangobi@gmail.com>
2026-05-24 16:16:24 +0800

    Switch to PDO for database connection

--- Questzania/dbConnect.php
+++ Questzania/dbConnect.php
@@ -5,9 +5,11 @@ $password = "UlkLxQTzEEzAIDbKWKQhpgKkOtoVobXR";
 $database = "railway";
 $port = 3306;
 
-$conn = mysqli_connect($host, $user, $password, $database, $port);
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$database", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = $pdo;
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
 }
 ?>
